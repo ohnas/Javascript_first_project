437578f54d7dda916b3c3b37762921b6bc46252e
ohnas <74810513+ohnas@users.noreply.github.com>
2022-03-20 18:40:18 +0900

    #6 style

--- back.js
+++ back.js
@@ -1,11 +1,10 @@
 const images = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg"];
 const body = document.querySelector("body");
-const img = document.createElement("img");
+const bg = document.querySelector(".bg");
 
 function randomBack(){
     const randomImage = images[Math.floor(Math.random() * images.length)];
-    img.src = `img/${randomImage}`;
-    body.appendChild(img);
+    bg.style.backgroundImage = `url(img/${randomImage})`;
 }
 
 randomBack();
